7b3f582bf60cc0b6f6551f6d8cd2315a55ae27c5
Gabriel Mochi Ribeiro <gmochi56@icloud.com>
2017-09-03 14:29:15 -0300

    Criação de abas de navegação; animação abas paralela ao menu

--- src/java/br/net/softwave/client/controller/MainController.java
+++ src/java/br/net/softwave/client/controller/MainController.java
@@ -1,14 +1,20 @@
 package br.net.softwave.client.controller;
 
 import br.net.softwave.client.Client;
+import br.net.softwave.client.util.ColorTransition;
+import br.net.softwave.client.util.ResizeWidthTransition;
+import com.jfoenix.controls.JFXHamburger;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.ParallelTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
@@ -18,6 +24,8 @@ public class MainController implements Initializable {
     @FXML private BorderPane main;
     @FXML private AnchorPane menu;
     @FXML private Circle perfil;
+    @FXML private ToolBar toolBar;
+    @FXML private JFXHamburger hamburger;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -31,15 +39,59 @@ public class MainController implements Initializable {
     
     @FXML
     private void onclickHambuger() {
-        TranslateTransition deslizaMenu = new TranslateTransition(Duration.millis(125), menu);
+        Duration duracao = new Duration(250);
+        Region centro = (Region) main.getCenter().lookup("JFXTabPane");
+        TranslateTransition deslizaMenu = new TranslateTransition(duracao, menu);
+        ResizeWidthTransition redimensionaCentro;
+        ParallelTransition parallelTransition;
         
         if (menu.getTranslateX() == 0) {
             deslizaMenu.setToX(-(menu.getWidth()));
-            deslizaMenu.play();
+            redimensionaCentro = new ResizeWidthTransition(duracao, centro, menu.getWidth() + centro.getWidth());
+            parallelTransition = new ParallelTransition(deslizaMenu, redimensionaCentro);
+            parallelTransition.play();
         } else {
             deslizaMenu.setToX(0);
-            deslizaMenu.play();
+            redimensionaCentro = new ResizeWidthTransition(duracao, centro, centro.getWidth() - menu.getWidth());
+            parallelTransition = new ParallelTransition(redimensionaCentro, deslizaMenu);
+            parallelTransition.play();
         }
+    }
+    
+    @FXML
+    private void abrirTelaForuns() {
+        ColorTransition ct = new ColorTransition("#4CAF50", toolBar, Duration.millis(500));
+        ct.play();
+        hamburger.lookupAll("StackPane").forEach((node) -> {
+            node.setStyle("-fx-background-color: black;");
+        });
+    }
+    
+    @FXML
+    private void abrirTelaAreasConhecimento() {
+        ColorTransition ct = new ColorTransition("#ffeb3b", toolBar, Duration.millis(500));
+        ct.play();
+        hamburger.lookupAll("StackPane").forEach((node) -> {
+            node.setStyle("-fx-background-color: black;");
+        });
+    }
+    
+    @FXML
+    private void abrirTelaChat() {
+        ColorTransition ct = new ColorTransition("#2196f3", toolBar, Duration.millis(500));
+        ct.play();
+        hamburger.lookupAll("StackPane").forEach((node) -> {
+            node.setStyle("-fx-background-color: white;");
+        });
+    }
+    
+    @FXML
+    private void abrirTelaAtividades() {
+        ColorTransition ct = new ColorTransition("#f44336", toolBar, Duration.millis(500));
+        ct.play();
+        hamburger.lookupAll("StackPane").forEach((node) -> {
+            node.setStyle("-fx-background-color: white;");
+        });
     }
     
 }
