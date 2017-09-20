82d9294f3ede9e960fa67baa555fc5bb9222f782
Gabriel Mochi Ribeiro <gmochi56@icloud.com>
2017-09-20 17:01:20 -0300

    Update Comentario.java

--- src/java/br/net/softwave/client/domain/Comentario.java
+++ src/java/br/net/softwave/client/domain/Comentario.java
@@ -1,16 +1,6 @@
-/*
- * Feito por John Schmitz Bretas
- * Use e abuse =)
- */
-
 package java.br.net.softwave.client.domain;
 
 import java.util.List;
-
-/** 
- * 
- * @author John Schmitz Bretas 
- */ 
 
 public class Comentario {
     private String texto;
